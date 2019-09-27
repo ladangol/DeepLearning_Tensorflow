940b0d8ae672819d9b1733aaf1656175360f901b
Shima Nikfal <snikfal@nidec-read.ca>
2019-09-27 18:26:40 -0400

    dogs_vs_cats:refactor simple_cnn.py

--- dogs_vs_cats/src/simple_cnn.py
+++ dogs_vs_cats/src/simple_cnn.py
@@ -1,3 +1,5 @@
+from util import getPath
+
 import keras
 from keras.layers import Dropout
 from keras.models import Sequential
@@ -20,27 +22,31 @@ from os import listdir
 from numpy import save, load
 import time
 
-data_dir = os.path.join("data","finalize_dogs_vs_cats")
 image_size = 224
 EPOCHS = 150
 num_classes = 2
 
-save_model = 'Models\simple_nn.model'
-save_label = 'Models\simple_nn_lb.pickle'
-save_plot = 'Models\simple_nn_plot.png'
-data_path = 'data/dogs-vs-cats'
-photoes_name = os.path.join(data_path, 'simple_dogs_vs_cats_photos.npy')
-labels_name = os.path.join(data_path, 'simple_dogs_vs_cats_labels.npy')
+model_path = 'models'
+data_path = 'data'
+
+save_model = getPath(model_path,'simple_nn.model')
+save_label = getPath(model_path,'simple_nn_lb.pickle')
+save_plot = getPath(model_path,'simple_nn_plot.png')
+
+photoes_name = getPath(data_path, 'simple_dogs_vs_cats_photos.npy')
+labels_name = getPath(data_path, 'simple_dogs_vs_cats_labels.npy')
+
+
+do_data_preparation = False
+do_train = True
+do_predict = False
 
 def prepare_data(in_data_dir, in_image_size):
     imagePaths = []
 
-    # define location of dataset
-    folder = os.path.join(data_path, 'train/')
-
     # enumerate files in the directory
-    for file in listdir(folder):
-        imagePath = os.path.join(folder, file)  # create path to dogs and cats
+    for file in listdir(in_data_dir):
+        imagePath = getPath(in_data_dir, file)  # create path to dogs and cats
         imagePaths.append(imagePath)
 
     random.seed(42)
@@ -50,7 +56,6 @@ def prepare_data(in_data_dir, in_image_size):
         image = cv2.imread(imagePath)
         image = cv2.resize(image, (in_image_size, in_image_size))
         data.append(image)
-        #label = imagePath.split(os.path.sep)[-2]
 
         # determine class
         output = 0.0
@@ -64,11 +69,11 @@ def prepare_data(in_data_dir, in_image_size):
     data = np.array(data, dtype="float") / 255.0
     labels = np.array(labels)
 
-    print(data.shape, labels.shape)
     # save the reshaped photos
     save(photoes_name, data)
     save(labels_name, labels)
 
+    print("Data preparation completed!")
     return data, labels
 
 
@@ -78,22 +83,14 @@ def define_model():
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(image_size, image_size, 3)))
     model.add(MaxPooling2D((2, 2)))
 
-   #model.add(Dropout(0.2))
-
     model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
     model.add(MaxPooling2D((2, 2)))
 
-    #model.add(Dropout(0.2))
-
     model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
     model.add(MaxPooling2D((2, 2)))
 
-    #model.add(Dropout(0.2))
-
     model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
     model.add(MaxPooling2D((2, 2)))
-
-    #model.add(Dropout(0.2))
 
     model.add(Flatten())
     model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
@@ -103,19 +100,17 @@ def define_model():
     model.add(Dense(num_classes, activation="softmax"))
     # compile model
     opt = SGD(lr=0.0001, momentum=0.9)
-    #opt='adam'
+
     model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
     model .summary()
     return model
 
 
-def predict_multi():
+def predict_multi(in_data_path, in_model_path):
     categories = ["Dog", "Cat"]
-    model_path = 'Models\\Model-48-0.814.model'
-    model = keras.models.load_model(model_path)
-    data_path = 'data\\dogs-vs-cats\\test'
-    for image_name in os.listdir(data_path):
-        image_path = os.path.join(data_path, image_name)
+    model = keras.models.load_model(in_model_path)
+    for image_name in os.listdir(in_data_path):
+        image_path = getPath(in_data_path, image_name)
         image = cv2.imread(image_path)
         image = cv2.resize(image, (image_size, image_size))
 
@@ -127,20 +122,12 @@ def predict_multi():
         plt.xlabel(className)
         plt.show()
 
-
-def main():
-    do_predict = True
-    if (do_predict):
-        predict_multi()
-        return
-
-    do_data_preparation = False
-    if(do_data_preparation):
-        data, labels = prepare_data(data_dir, image_size)
-
+def train():
+    print("Loading data!")
     data = load(photoes_name)
     labels = load(labels_name)
 
+    print("Preprocessing data!")
     (trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.25, random_state=42)
 
     trainY = keras.utils.to_categorical(trainY, num_classes)
@@ -152,7 +139,10 @@ def main():
     filepath = "Model-{epoch:02d}-{val_acc:.3f}"  # unique file name that will include the epoch and the validation acc for that epoch
     checkpoint = ModelCheckpoint("Models/{}.model".format(filepath, monitor='val_acc', verbose=1, save_best_only=True,
                                                           mode='max'))  # saves only the best ones
-    tensorBoard = TensorBoard(log_dir='Models\logs\{}'.format(NAME))
+    log_path = getPath(model_path, 'logs')
+    log_file_name = '{}'.format(NAME)
+    log_full_path = getPath(log_path, log_file_name)
+    tensorBoard = TensorBoard(log_dir=log_full_path)
 
     early_stop = EarlyStopping(monitor='val_loss', patience=1, verbose=1, mode='auto')
     callback_list = [checkpoint, tensorBoard]
@@ -167,23 +157,20 @@ def main():
     print(classification_report(testY.argmax(axis=1),
                                 predictions.argmax(axis=1), target_names=num_classes))
 
-    # # plot the training loss and accuracy
-    # N = np.arange(0, EPOCHS)
-    # plt.style.use("ggplot")
-    # plt.figure()
-    # plt.plot(N, H.history["loss"], label="train_loss")
-    # plt.plot(N, H.history["val_loss"], label="val_loss")
-    # plt.plot(N, H.history["acc"], label="train_acc")
-    # plt.plot(N, H.history["val_acc"], label="val_acc")
-    # plt.title("Training Loss and Accuracy (Simple NN)")
-    # plt.xlabel("Epoch #")
-    # plt.ylabel("Loss/Accuracy")
-    # plt.legend()
-    # plt.savefig(save_plot)
-    #
-    # # save the model and label binarizer to disk
-    # print("[INFO] serializing network and label binarizer...")
-
     model.save(save_model)
+
+def main():
+    if(do_data_preparation):
+        # define location of dataset
+        train_data_path = getPath(data_path, 'train')
+        prepare_data(train_data_path, image_size)
+
+    if(do_train):
+        train()
+
+    if (do_predict):
+        test_model_path = getPath(model_path,'Model-48-0.814.model')
+        test_data_path = getPath(data_path, 'test')
+        predict_multi(test_data_path, test_model_path)
 
 main()
